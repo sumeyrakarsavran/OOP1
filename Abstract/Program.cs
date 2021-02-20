2d463952f108cb0dc064a56cc80fa0be21b1b48e
sumeyrakarsavran <bir-kar-12@hotmail.com>
2021-02-20 11:13:48 +0300

    addded..

---
+++ Abstract/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MyDictionery/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Odev4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+        }
+    }
+}
